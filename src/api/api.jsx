a1cdf9088dcbd99d6edfa24f3c976a28aa5668a0
Oksana-Marchenko11 <oksana.marchenko1988@gmail.com>
2023-10-01 20:03:34 +0300

    new

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,36 +1,17 @@
 import axios from 'axios';
+import { App } from '../components/App';
 
-axios.defaults.baseURL =
-  'https://pixabay.com/api/?q=cat&page=1&key=38733700-96318c553c84bf90463eb3752&image_type=photo&orientation=horizontal&per_page=12';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-export const getItems = async (myState, setState, searchName, currentPage) => {
-  // console.log(myState.images);
-  try {
-    setState({ isLoading: true });
-    const response = await axios.get(axios.defaults.baseURL, {
-      params: {
-        q: searchName,
-        per_page: 12,
-        page: currentPage,
-      },
-    });
-    setState({ isLoading: false });
-    const images = response.data.hits;
-    // console.log(response.data.total);
-    // console.log(response.config.params.per_page);
-
-    setState({
-      totalPages: Number(
-        Math.ceil(response.data.total / response.config.params.per_page)
-      ),
-    });
-    if (currentPage === 1) {
-      setState({ images: images });
-    } else {
-      setState({ images: [...myState.images, ...images] });
-    }
-    return images;
-  } catch (error) {
-    console.error(error);
-  }
+export const getItems = async ({ searchName, currentPage }) => {
+  const response = await axios.get(axios.defaults.baseURL, {
+    params: {
+      q: searchName,
+      per_page: 12,
+      page: currentPage,
+      key: '38733700-96318c553c84bf90463eb3752',
+    },
+  });
+  console.log(response.data.hits);
+  return response.data.hits;
 };
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,32 +17,34 @@ export class App extends Component {
   };
   handleSubmit = searchValue => {
     this.setState({ searchName: searchValue });
-    this.setState({ currentPage: 1 });
-    console.log(this.state);
-    // console.log(this.state.totalPages);
+    console.log(this.state.searchName);
+    console.log(searchValue);
+    this.setState({ currentPage: 1, images: [] });
   };
   loadMore = () => {
-    this.setState(prevState => ({
-      currentPage: prevState.currentPage + 1,
-    }));
-    // console.log(this.state.currentPage);
-    console.log(this.state);
+    this.setState({
+      currentPage: this.state.currentPage + 1,
+    });
   };
-  componentDidUpdate(_, prevState) {
+  async componentDidUpdate(prevProps, prevState) {
+    const { searchName, currentPage } = this.state;
     if (
       prevState.searchName !== this.state.searchName ||
       prevState.currentPage !== this.state.currentPage
     ) {
-      getItems(
-        this.state,
-        this.setState.bind(this),
-        this.state.searchName,
-        this.state.currentPage
-      );
-      this.setState({ isLoading: true });
+      try {
+        this.setState({ isLoading: true });
+        console.log(searchName);
+        const result = await getItems({ searchName, currentPage });
+        this.setState({ images: [...this.state.images, ...result] });
+        console.log(this.state);
+      } catch (e) {
+        console.log('error');
+      } finally {
+        this.setState({ isLoading: false });
+      }
     }
   }
-
   render() {
     const { images, currentPage, totalPages, isLoading } = this.state;
     return (
